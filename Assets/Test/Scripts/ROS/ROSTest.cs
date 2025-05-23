ab058ebaaaf9ef8bae6b72f84e677061b568a94a
katsumasa <katsumasa-abe@outlook.jp>
2025-05-23 17:00:09 +0900

    Add RosTest katsumasa

---
+++ Assets/Test/Scripts/ROS/ROSTest.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using RosSharp.RosBridgeClient.MessageTypes.Sensor;
+using UnityEngine;
+
+public class ROSTest : MonoBehaviour
+{
+    private IMUSubscriber imuSubscriber;
+    private RotationSubscriber rotationSubscriber;
+    private StrengthPublisher strengthPublisher;
+    private Dictionary<string, Imu> imus;
+    private Dictionary<string, float> rotations;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        imuSubscriber = GetComponent<IMUSubscriber>();
+        rotationSubscriber = GetComponent<RotationSubscriber>();
+        strengthPublisher = GetComponent<StrengthPublisher>();
+        imus = imuSubscriber.GetImus();
+        rotations = rotationSubscriber.GetRotations();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}

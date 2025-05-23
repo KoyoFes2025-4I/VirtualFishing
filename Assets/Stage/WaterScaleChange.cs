262061a7d86068c4ebf9160632e4cf58d7d74ecc
32230Tsuzuki-EE <133293700+32230Tsuzuki-EE@users.noreply.github.com>
2025-05-23 14:00:58 +0900

    [add]WaterScaleChange

--- Assets/Stage/WaterScaleChange.cs
+++ Assets/Stage/WaterScaleChange.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-/*カメラの形式を間違えていたスクリプト*/
 
 public class WaterScaleChange : MonoBehaviour
 {
@@ -8,6 +7,7 @@ public class WaterScaleChange : MonoBehaviour
     Camera cam1, cam2;
 
     const float PLANE_SIZE = 10f;
+    [SerializeField]private float cameraY = 1000f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -23,19 +23,22 @@ public class WaterScaleChange : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        WaterFloorScaleChange(cam1obj, waterFloor);
+        WaterFloorScaleChange(cam1obj, cam2obj, waterFloor, cameraY);
     }
 
-    void WaterFloorScaleChange(GameObject camera, GameObject water)
+    void WaterFloorScaleChange(GameObject camera1, GameObject camera2, GameObject water ,  float camY)
     {
-        Camera cam = camera.GetComponent<Camera>();
+        Camera cam1 = camera1.GetComponent<Camera>();
+        Camera cam2 = camera2.GetComponent<Camera>();
 
-        float height = cam.orthographicSize * 2f;
-        float width = height * cam.aspect;
+        float height = cam1.orthographicSize * 2f;
+        float width = height * cam1.aspect;
 
-        float scaleX = width / PLANE_SIZE;
+        float scaleX = (width * 2) / PLANE_SIZE;
         float scaleZ = height / PLANE_SIZE;
 
         water.transform.localScale = new Vector3(scaleX, 1f, scaleZ);
+        camera1.transform.position = new Vector3(-width / 2, camY, 0f);
+        camera2.transform.position = new Vector3(width / 2, camY, 0f);
     }
 }

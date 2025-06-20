56a2cda3ed6a6a27d946eb8f9bb16095bfcc24e5
YuiAsakura <sirokogu333@gmail.com>
2025-06-20 14:06:53 +0900

    ControllerSceneの追加
    ContorollerScene, Script フォルダの追加、不要なファイルを削除

--- Assets/Controller/Scripts/Rod/RodScript.cs
+++ Assets/Controller/Scripts/Rod/RodScript.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class YuyaTakakura : MonoBehaviour
+public class RodScript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()

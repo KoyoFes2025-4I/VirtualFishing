6d7edd99c7bfd4a5449210ce4bf79d8f3aa697a7
katsumasa <katsumasa-abe@outlook.jp>
2025-07-11 11:39:46 +0900

    リファクタリングと引き上げ処理追加

--- Assets/Controller/Scripts/Rod/RodScript.cs
+++ Assets/Controller/Scripts/Rod/RodScript.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using RosSharp;
 using RosSharp.RosBridgeClient.MessageTypes.Sensor;
 using UnityEngine;
@@ -15,12 +16,13 @@ public class RodScript : MonoBehaviour
     [SerializeField]
     private StrengthPublisher strengthPublisher;
     [SerializeField]
-    private GameObject ball;
-    private float baseRotationY;
+    private GameObject bite;
+    private float baseRotationY = 0;
     private string id = "";
     private float maxMagnitude = -1;
-    private DateTime rodTime;
-    private bool isThrowing;
+    private DateTime rodTime = DateTime.Now;
+    private bool isThrowing = false;
+    private bool isThrown = false;
     private float thresholdMagnitude = 5;
     private float throwingTime = 1;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -40,45 +42,69 @@ public class RodScript : MonoBehaviour
         baseRotationY = y;
     }
 
-    void CheckFeed()
+    public void SetThresholdMagnitude(float magnitude)
     {
-        Vector3 accel = new Vector3((float)imus[id].linear_acceleration.x, (float)imus[id].linear_acceleration.y, (float)imus[id].linear_acceleration.z);
-        if(accel.magnitude > thresholdMagnitude)
-        {
-            if(!isThrowing){
-                rodTime = DateTime.Now;
-                isThrowing = true;
-            }
-            if (maxMagnitude < accel.magnitude)
-            {
-                maxMagnitude = accel.magnitude;
-            }
-        }
-        if (((DateTime.Now - rodTime).TotalSeconds > throwingTime) && isThrowing)
-        {
-            isThrowing = false;
-            Vector3 bite = transform.up;
-            bite.y = 0;
-            bite = bite.normalized * -maxMagnitude;
-            ball.SetActive(true);
-            ball.GetComponent<Rigidbody>().linearVelocity = bite;
-            maxMagnitude = -1;
-        }
+        thresholdMagnitude = magnitude;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetThrowingTime(float time)
+    {
+        throwingTime = time;
+    }
+
+    void SetOrientation()
     {
         try
         {
             Quaternion zRotation = Quaternion.AngleAxis(90f, Vector3.forward);
             Quaternion yRotation = Quaternion.AngleAxis(baseRotationY, Vector3.up);
             transform.rotation = yRotation * zRotation * new Quaternion((float)imus[id].orientation.x, (float)-imus[id].orientation.y, (float)imus[id].orientation.z, (float)imus[id].orientation.w);
-            CheckFeed();
-        }
-        catch(Exception e)
+        } catch (Exception) {}
+    }
+
+    void CheckFeed()
+    {
+        try
         {
-            Debug.Log(e);
+            Vector3 accel = new Vector3((float)imus[id].linear_acceleration.x, (float)imus[id].linear_acceleration.y, (float)imus[id].linear_acceleration.z);
+            if (accel.magnitude > thresholdMagnitude)
+            {
+                if (!isThrowing)
+                {
+                    rodTime = DateTime.Now;
+                    isThrowing = true;
+                }
+                maxMagnitude = Math.Max(maxMagnitude, accel.magnitude);
+            }
+            if (((DateTime.Now - rodTime).TotalSeconds > throwingTime) && isThrowing)
+            {
+                if (!isThrown)
+                {
+                    isThrowing = false;
+                    Vector3 biteVelocity = transform.up;
+                    biteVelocity.y = 0;
+                    biteVelocity = biteVelocity.normalized * -maxMagnitude;
+                    bite.SetActive(true);
+                    bite.GetComponent<Rigidbody>().linearVelocity = biteVelocity;
+                    maxMagnitude = -1;
+                    isThrown = true;
+                }
+                else
+                {
+                    bite.SetActive(false);
+                    bite.GetComponent<Rigidbody>().linearVelocity = Vector3.zero;
+                    bite.transform.position = transform.position + new Vector3(0, 10, 0);
+                    isThrown = false;
+                }
+            }
         }
+        catch (Exception) { }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetOrientation();
+        CheckFeed();
     }
 }

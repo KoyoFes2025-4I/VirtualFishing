c8b98077c3357598a2cd9de494e9e5cda62278a6
emuenu <107606113+emuenu@users.noreply.github.com>
2025-07-16 13:13:03 +0900

    fix: バグを修正

--- Assets/Fish/Scripts/For_Feed/FeedPositionManager.cs
+++ Assets/Fish/Scripts/For_Feed/FeedPositionManager.cs
@@ -3,16 +3,10 @@ using UnityEngine;
 public class FeedPositionManager : MonoBehaviour
 {
 
-    private Vector3 FeedPosition; // 餌オブジェクトの位置
+    [SerializeField] private Vector3 FeedPosition; // 餌オブジェクトの位置
 
     // 着水時にコントローラーから「釣り竿をどれだけ強く振ったか」のような数値を返してもらう
     // その情報から餌の位置のFeedPosition（3次元ベクトル）を計算して更新させるようにする
-
-    private void Start()
-    {
-
-        FeedPosition = new Vector3(300, 0, 0); // テスト用に固定位置を設定
-    }
 
     private void Update()
     {
--- Assets/Fish/Scripts/For_Fish/FishBattle.cs
+++ Assets/Fish/Scripts/For_Fish/FishBattle.cs
@@ -8,25 +8,39 @@ public class FishBattle : MonoBehaviour
 
     private ThingsToFish fish;
 
+    private int strength;
+    private int power;
+    private bool isBattling = false;
+
+    public void Battle(int FishStrength, int FishPower)
+    {
+        strength = FishStrength;
+        power = FishPower;
+        isBattling = true; // Battle関数が呼び出された時下記のUpdate関数をアクティブ化
+    }
+
     void Start()
     {
         fish = GetComponent<ThingsToFish>();
     }
 
-    public void Battle(int FishStrength, int FishPower)
+    private void Update()
     {
-        // 50%ずつで勝敗が決まる設定
-        if (Random.Range(-1f, 1f) >= 0.5f)
+        if (!isBattling) return; // isBattlingがfalseだと何もしない
+
+        // テスト用にWキーを押したら勝利
+        if (Input.GetKeyDown(KeyCode.W))
         {
-            fish.SetSuccessFishing(); // 勝利フラグを立てておく
-            fish.SetFinishFishing(); // バトル終了（勝利時）
+            fish.SetSuccessFishing(); // 勝利フラグを立てる
+            fish.SetFinishFishing(); // バトル終了（勝利）
+            isBattling = false;
         }
 
-        else
+        // テスト用にLキーを押したら敗北
+        else if (Input.GetKeyDown(KeyCode.L))
         {
-            fish.SetFinishFishing(); // バトル終了（敗北時）
+            fish.SetFinishFishing(); // バトル終了（敗北）
+            isBattling = false;
         }
-            
     }
-
 }

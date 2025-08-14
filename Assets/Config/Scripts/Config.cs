4bb4044f5e4d00228aedade03f5b07a1885a309b
katsumasa1234 <katsumasa-abe@outlook.jp>
2025-08-14 21:00:32 +0900

    コンフィグのセーブロード機能追加

--- Assets/Config/Scripts/Config.cs
+++ Assets/Config/Scripts/Config.cs
@@ -1,4 +1,9 @@
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEditor.Rendering;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.UIElements;
 
 public class Config : MonoBehaviour
@@ -51,6 +56,25 @@ public class Config : MonoBehaviour
 
         applyButton = ui.rootVisualElement.Q<Button>("ApplyButton");
         applyButton.clicked += Apply;
+
+
+        ConfigSaveData config = new ConfigSaveData();
+        config.Load();
+
+        configCameraSpeedField.value = config.configCameraSpeed;
+        configCameraDashSpeedField.value = config.configCameraDashSpeed;
+        cameraSensitivityField.value = config.configCameraSensitivity;
+
+        rodDropDown.index = config.rodCountIndex;
+        controllerRotationYField.value = config.baseRotationY;
+        throwPowerField.value = config.throwPower;
+        rodPowerField.value = config.rodPower;
+        maxRodStrengthField.value = config.maxRodStrength;
+        rodIDDropDown.index = config.rodIDGenerateIndex;
+        rodUIScale.value = config.rodUIScale;
+
+        stageSizeField.value = (uint)config.stageSize;
+        cameraYField.value = (uint)config.cameraY;
     }
 
     private void Apply()
@@ -60,11 +84,32 @@ public class Config : MonoBehaviour
         rodsController.ChangeParams(rodDropDown.index, controllerRotationYField.value, throwPowerField.value, rodPowerField.value, maxRodStrengthField.value, rodIDDropDown.index, rodUIScale.value);
 
         thingGenerator.Regenerate();
+
+
+        ConfigSaveData config = new ConfigSaveData();
+
+        config.configCameraSpeed = configCameraSpeedField.value;
+        config.configCameraDashSpeed = configCameraDashSpeedField.value;
+        config.configCameraSensitivity = cameraSensitivityField.value;
+
+        config.rodCountIndex = rodDropDown.index;
+        config.baseRotationY = controllerRotationYField.value;
+        config.throwPower = throwPowerField.value;
+        config.rodPower = rodPowerField.value;
+        config.maxRodStrength = maxRodStrengthField.value;
+        config.rodIDGenerateIndex = rodIDDropDown.index;
+        config.rodUIScale = rodUIScale.value;
+
+        config.stageSize = (int)stageSizeField.value;
+        config.cameraY = (int)cameraYField.value;
+
+        config.Save();
     }
 
     void Start()
     {
         FieldInit();
+        Apply();
     }
 
     void Update()
@@ -74,6 +119,63 @@ public class Config : MonoBehaviour
         if (configCamera.locked) UnityEngine.Cursor.lockState = CursorLockMode.Locked;
         else UnityEngine.Cursor.lockState = CursorLockMode.None;
 
+        bool prev = ui.enabled;
         ui.enabled = !configCamera.locked;
+        if (!prev && ui.enabled) FieldInit();
+    }
+}
+
+
+
+
+[Serializable]
+public class ConfigSaveData
+{
+    public static string path = Path.Combine(Application.persistentDataPath, "config.vf");
+
+    public float configCameraSpeed = 5f;
+    public float configCameraDashSpeed = 15f;
+    public float configCameraSensitivity = 500f;
+
+    public int rodCountIndex = 0;
+    public float baseRotationY = 0;
+    public float throwPower = 0.3f;
+    public float rodPower = 0.3f;
+    public float maxRodStrength = 100;
+    public float rodUIScale = 1;
+    public int rodIDGenerateIndex = 0;
+
+    public int stageSize = 10;
+    public int cameraY = 200;
+
+    public void Save()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        using (FileStream fs = File.Create(path)) bf.Serialize(fs, this);
+    }
+
+    public bool Load()
+    {
+        if (!File.Exists(path)) return false;
+
+        BinaryFormatter bf = new BinaryFormatter();
+        ConfigSaveData tmp;
+        using (FileStream fs = File.Open(path, FileMode.Open)) tmp = (ConfigSaveData)bf.Deserialize(fs);
+
+        configCameraSpeed = tmp.configCameraSpeed;
+        configCameraDashSpeed = tmp.configCameraDashSpeed;
+        configCameraSensitivity = tmp.configCameraSensitivity;
+
+        rodCountIndex = tmp.rodCountIndex;
+        baseRotationY = tmp.baseRotationY;
+        throwPower = tmp.throwPower;
+        rodPower = tmp.rodPower;
+        maxRodStrength = tmp.maxRodStrength;
+        rodUIScale = tmp.rodUIScale;
+        rodIDGenerateIndex = tmp.rodIDGenerateIndex;
+
+        stageSize = tmp.stageSize;
+        cameraY = tmp.cameraY;
+        return true;
     }
 }
